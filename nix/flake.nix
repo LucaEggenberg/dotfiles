e890ceaede3404fadb9e1a1357ea04857bb6a2a3
Luca Eggenberg <luca.e@outlook.com>
2025-07-31 20:31:59 +0200

    correctly expose function

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,18 +5,20 @@
         nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, ... }: 
-    let
-        dotfile-dir = "$self/.stow-targets";
-    in {
-        home.file = {
-            ".config/btop".source = "${dotfile-dir}/btop";
-            ".config/hypr".source = "${dotfile-dir}/hypr";
-            ".config/kitty".source = "${dotfile-dir}/kitty";
-            ".config/mako".source = "${dotfile-dir}/mako";
-            ".config/waybar".source = "${dotfile-dir}/waybar";
-            ".config/wlogout".source = "${dotfile-dir}/wlogout";
-            ".config/wofi".source = "${dotfile-dir}/wofi";
+    outputs = { self, nixpkgs, ... }: {
+        homeManagerModules.dotfiles = { config, pkgs, ... }:
+        let
+            dotfile-dir = "$self/.stow-targets";
+        in {
+            home.file = {
+                ".config/btop".source = "${dotfile-dir}/btop";
+                ".config/hypr".source = "${dotfile-dir}/hypr";
+                ".config/kitty".source = "${dotfile-dir}/kitty";
+                ".config/mako".source = "${dotfile-dir}/mako";
+                ".config/waybar".source = "${dotfile-dir}/waybar";
+                ".config/wlogout".source = "${dotfile-dir}/wlogout";
+                ".config/wofi".source = "${dotfile-dir}/wofi";
+            };
         };
     };
 }
