3596243ccb3e4e09816dddd15d8dff722f25ee05
Luca Eggenberg <luca.e@outlook.com>
2025-07-31 22:45:54 +0200

    try recursively linking entire .config

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,13 +11,10 @@
             dotfile-dir = "${self}/.stow-targets";
         in {
             home.file = {
-                ".config/btop".source = "${dotfile-dir}/.config/btop";
-                ".config/hypr".source = "${dotfile-dir}/.config/hypr";
-                ".config/kitty".source = "${dotfile-dir}/.config/kitty";
-                ".config/mako".source = "${dotfile-dir}/.config/mako";
-                ".config/waybar".source = "${dotfile-dir}/.config/waybar";
-                ".config/wlogout".source = "${dotfile-dir}/.config/wlogout";
-                ".config/wofi".source = "${dotfile-dir}/.config/wofi";
+                ".config" = {
+                    source = "${dotfile-dir}/.config";
+                    recursive = true;
+                };
             };
         };
     };
